9421c7b41e9e267bd59f0323ff8841609fa9dcd5
lucasjs <lucas.tcjb@gmail.com>
2018-08-22 21:19:26 -0300

    chore: add environment urls

--- packages/pilot/src/environment.js
+++ packages/pilot/src/environment.js
@@ -1,5 +1,14 @@
-const { REACT_APP_API_ENVIRONMENT } = process.env
+const {
+  REACT_APP_API_ENVIRONMENT,
+  REACT_APP_LIVE_URL = 'https://beta.dashboard.stg.pagarme.net/latest/index.html',
+  REACT_APP_TEST_URL = 'https://beta.dashboard.sandbox.stg.pagarme.net/latest/index.html',
+} = process.env
 
 const env = REACT_APP_API_ENVIRONMENT === 'live' ? 'live' : 'test'
+
+export {
+  REACT_APP_LIVE_URL,
+  REACT_APP_TEST_URL,
+}
 
 export default env
